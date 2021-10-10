ee99f58f0079bb4d8b9ce33c7bcfaca622dee278
mooezz999 <moustafaezz999@gmail.com>
2021-10-10 21:34:55 +0200

    mongo db

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+//CONNECT TO MONGODB
+const dbURI = "mongodb+srv://netninja:Mm123456@cluster0.2jyrm.mongodb.net/node-tuts?retryWrites=true&w=majority";
+mongoose.connect(dbURI)
+.then((result)=>app.listen(3000))
+.catch((err) => console.log(err));
 //express app
 const app = express();
 
@@ -8,21 +15,43 @@ app.set('view engine', 'ejs');
 
 
 // listen for requests
-app.listen(3000);
+
 //middleware & static files
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
 
+app.get('/add-blog',(req,res) => {
+const blog = new Blog({
+    title: 'new blog 2',
+    snippet: 'about my new blog 2',
+    body: 'more about my new blog 2'
+});
+
+blog.save()
+.then((result) =>{
+res.send(result)
+})
+.catch((err) => {
+    console.log(err);
+});
+
+});
+
+app.get('/all-blogs',(req,res)=>{
+    Blog.find()
+    .then((result)=>{
+        res.send(result);
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+})
+
 app.get('/',(req,res)=>{
 // res.send('<p>home page</p>');
-const blogs = [
-{title:'yoshi finds eggs', snippet:'lorem ipsum'},
-{title:'ggggg', snippet:'kkkkkkkk'},
-{title:'jjjjjjjjjjj', snippet:'hjjjjj'}
-];
-res.render('index', { title: 'Home', blogs});
+res.redirect('/blogs');
 });
 
 app.get('/about',(req,res)=>{
@@ -35,6 +64,17 @@ app.get('/about',(req,res)=>{
         res.render('about',{title: 'About'});
         });
 
+
+        //blog routes
+        app.get('/blogs', (req,res) => {
+            Blog.find()
+            .then((result)=>{
+            res.render('index', {title:'All Blogs', blogs: result})
+            })
+            .catch((err)=>{
+            console.log(err);
+            });
+        })
         //create blog
         app.get('/blogs/create',(req,res)=>{
             res.render('create',{title:'create a new blog'});
